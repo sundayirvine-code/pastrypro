413ba4ec9ef11970ede51a6ef33ff90cde657c55
Irvine Sunday <amugumbiirvine@gmail.com>
2023-05-22 15:04:02 +0300

    define routres for signup and login

--- app/routes.py
+++ app/routes.py
@@ -10,9 +10,18 @@ bp = Blueprint('routes', __name__)
 def home():
     return render_template('index.html')
 
-@bp.route('/products')
+# Define routes for signup and login
+@bp.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@bp.route('/login')
+def login():
+    return render_template('login.html')
+
+@bp.route('/inventory')
 def products():
     # Logic to fetch and display products
-    return render_template('products.html')
+    return render_template('inventory.html')
 
 # Add more routes as needed
