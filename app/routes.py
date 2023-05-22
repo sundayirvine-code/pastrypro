c0647f2b8b8297292a455671f4120b0d9c3e37bb
Irvine Sunday <amugumbiirvine@gmail.com>
2023-05-22 15:27:47 +0300

    installed Flask-WTF WTForms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # routes.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from .forms import RegistrationForm, LoginForm
 
 # Create a blueprint for the routes
 bp = Blueprint('routes', __name__)
@@ -11,13 +12,23 @@ def home():
     return render_template('index.html')
 
 # Define routes for signup and login
-@bp.route('/signup')
+@bp.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Process the form data (e.g., create a new user)
+        # Redirect to a success page or login page
+        return redirect(url_for('routes.login'))
+    return render_template('signup.html', form=form)
 
-@bp.route('/login')
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Process the form data (e.g., authenticate user)
+        # Redirect to a logged-in page or dashboard
+        return redirect(url_for('routes.home'))
+    return render_template('login.html', form=form)
 
 @bp.route('/inventory')
 def products():
