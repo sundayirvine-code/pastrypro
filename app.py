1af82b99ba698ca774613f427c7532bb489b64a1
Irvine Sunday <amugumbiirvine@gmail.com>
2023-05-23 10:10:28 +0300

    restructured my project. moved everythin in app.py

---
+++ app.py
@@ -0,0 +1,103 @@
+# app.py
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for, request, flash
+from forms import RegistrationForm, LoginForm
+
+
+app = Flask(__name__)
+
+# Configuration
+app.config['SECRET_KEY'] = 'your-secret-key-here'  # Replace with your secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Database setup
+db = SQLAlchemy(app)
+
+
+#models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    # Define other user fields as needed
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, default=0)
+    image_id = db.Column(db.Integer, db.ForeignKey('image.id'))
+    # Define other product fields as needed
+    
+    def __repr__(self):
+        return f"Product('{self.name}', '{self.price}')"
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(255), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    
+    def __repr__(self):
+        return f"Image('{self.image_url}')"
+    
+
+# Define your routes
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Define routes for signup and login
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Create a new user based on form data
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        print(user)
+        db.session.add(user)
+        db.session.commit()
+        # Redirect to a success page or login page
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():  
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Check if the user exists and password is correct
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password == form.password.data:
+            # Redirect to a logged-in page or dashboard
+            return redirect(url_for('inventory'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/inventory')
+def products():
+    # Logic to fetch and display products
+    return render_template('inventory.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        #create tables
+        db.create_all()
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-
-import os
-
-class Config:
-    SECRET_KEY = 'your-secret-key'
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///your-database-file.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
