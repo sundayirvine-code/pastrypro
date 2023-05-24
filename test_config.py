c792ecf98843741f9d216b10608449a6e59e2bf6
Irvine Sunday <amugumbiirvine@gmail.com>
2023-05-24 14:31:13 +0300

    created tests

---
+++ test_config.py
@@ -0,0 +1,6 @@
+# test_config.py
+
+class TestConfig:
+    SECRET_KEY = 'your-test-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
