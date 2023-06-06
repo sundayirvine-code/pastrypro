8e0b2f326c847022f4af84150da51c5af15caecc
Irvine Sunday <amugumbiirvine@gmail.com>
2023-06-06 14:54:08 +0300

    updated the login form to remove autocomplete feature

--- forms.py
+++ forms.py
@@ -3,13 +3,13 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Username', validators=[DataRequired()], render_kw={"autocomplete": "off"})
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"autocomplete": "off"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"autocomplete": "off"})
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')], render_kw={"autocomplete": "off"})
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"autocomplete": "off"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"autocomplete": "off"})
     submit = SubmitField('Log In')
