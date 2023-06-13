23fa72c89731209d4b5cb4965d931e0b6b186db5
Irvine Sunday <amugumbiirvine@gmail.com>
2023-06-13 05:03:58 +0300

    deleted test files and updated the requirements file

--- test_config.py
+++
@@ -1,6 +0,0 @@
-# test_config.py
-
-class TestConfig:
-    SECRET_KEY = 'your-test-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_database.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
